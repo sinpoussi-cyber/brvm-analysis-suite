ee2a38d297471d06a57ac4fb04b5ac25ed912e31
sinpoussi-cyber <sinpoussi@gmail.com>
2025-09-27 10:26:12 +0000

    Update fundamental_analyzer.py

--- fundamental_analyzer.py
+++ fundamental_analyzer.py
@@ -1,7 +1,6 @@
 # ==============================================================================
 # MODULE: FUNDAMENTAL ANALYZER (V3.2 - CORRECTION NOM MODÈLE)
 # ==============================================================================
-
 import requests
 from bs4 import BeautifulSoup
 import time
@@ -25,8 +24,6 @@ import google.generativeai as genai
 from google.api_core import exceptions as api_exceptions
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-# --- Configuration & Secrets ---
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s: %(message)s')
 DB_NAME = os.environ.get('DB_NAME')
 DB_USER = os.environ.get('DB_USER')
